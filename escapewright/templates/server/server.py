0b30c14fa3e61e11f12331ab079ae838a05d2604
Lorenzo Moon <lodomo@lodomo.dev>
2023-12-29 17:19:39 -0800

    Servers now create a new log file at midnight.

--- escapewright/templates/server/server.py
+++ escapewright/templates/server/server.py
@@ -13,6 +13,8 @@ import sys                     # Used to exit the program during critical errors
 import importlib               # Used to import the role
 import logging                 # Used to log the program
 from datetime import datetime  # Used to get the current date
+import time
+import threading
 
 # Internal Imports
 from escapewright.escapiserver import EscapiServer
@@ -26,6 +28,9 @@ def main():
 
     logger = set_logger(info)            # Set the logger, if logging is enabled
     disable_flask_log()                  # Disable Flask's default logger
+
+    midnight_thread = threading.Thread(target=update_logger_file, args=(logger,), daemon=True)
+    midnight_thread.start()
 
     name = info['name']                  # Set Server Name
     role = create_role(info, logger)     # Set Server Role
@@ -122,6 +127,48 @@ def disable_flask_log():
     flask_log = logging.getLogger('werkzeug')
     flask_log.disabled = True
     return 
+
+def seconds_to_midnight():
+    # Get the current date
+    current_date = datetime.now()
+    # Get the the time until midnight
+    time_to_midnight = datetime(current_date.year, current_date.month, current_date.day, 0, 0, 0) - current_date
+    # Convert the time to seconds
+    seconds_until_midnight = time_to_midnight.seconds
+    return time_to_midnight 
+
+def update_logger_file(logger):
+    if logger is None:
+        return
+    
+    while True:
+        logger.log(logging.INFO, "A welcome start to a new day")
+        sleep_time = seconds_to_midnight()
+        time.sleep(sleep_time)
+        logger.log(logging.INFO, f"N.F.E.T.P.")
+
+        # Remove all handlers associated with the logger
+        for handler in logger.handlers[:]:
+            logger.removeHandler(handler)
+
+        log_base_dir = "EW.Logs" # Base directory for the logs
+        # Get the current date for the directory structure
+        # YYYY/MM/DD.ewlog
+        current_date = datetime.now()
+        year_dir = os.path.join(log_base_dir, str(current_date.year))
+        month_dir = os.path.join(year_dir, f"{current_date.month:02d}")
+        # Make the file name DDHHMMSS.ewlog
+        day_file = os.path.join(month_dir, f"{current_date.day:02d}.ewlog")
+
+        # Create a new file handler with the new log file name
+        new_handler = logging.FileHandler(day_file)
+
+        # Set the log format
+        formatter = logging.Formatter('[%(asctime)s] %(levelname)s: %(message)s', datefmt='%m-%d %H:%M:%S')
+        new_handler.setFormatter(formatter)
+
+        # Add the new handler to the logger
+        logger.addHandler(new_handler)
 
 if __name__ == "__main__":
     main()
