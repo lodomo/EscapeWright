01f63becad11a7d9d118cccc6bb2951c9b48bdae
Lodomo <lodomo.dev@gmail.com>
2024-11-10 00:07:20 -0800

    Setup some of the install files

--- ControlPanel/setup.sh
+++ ControlPanel/setup.sh
@@ -3,3 +3,8 @@
 echo 'This will setup the environment for the project.'
 echo 'It is NOT working yet.'
 echo 'TODO'
+echo "This has not been tested on a fresh install yet"
+
+# Prepare the API and it's dependencies
+cd api
+./prep_environment.sh
